685dbeb321d13ae55225a09ac5415e2613966823
Favourene <igbinosaiwinosa01@gmail.com>
2023-06-17 21:00:28 +0100

    new

---
+++ src/Home.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import './App.scss'
+import Logo from './images/logo.jpg'
+import Teen from './images/teen.jpg'
+import Teen2 from './images/teen2.jpg'
+import Frame from './images/frame.png'
+import { useNavigate } from 'react-router-dom'
+
+function Home() {
+  const List = [
+    {
+      name: 'Culinary',
+      tutor: 'Demola	',
+    },
+    {
+      name: 'Nail Techinician',
+      tutor: 'Bidemi	',
+    },
+    {
+      name: 'Skin Care',
+      tutor: 'Jane	',
+    },
+    {
+      name: 'Learning Instruments',
+      tutor: 'Tomiwa/princewil',
+    },
+    {
+      name: 'Fashion Designing',
+      tutor: 'Kemah	',
+    },
+    {
+      name: 'Hair Styling',
+      tutor: '?',
+    },
+    {
+      name: 'Digital marketing',
+      tutor: 'Wale	',
+    },
+    {
+      name: 'Content Writing',
+      tutor: 'Gloria	',
+    },
+    {
+      name: 'Public Speaking',
+      tutor: 'P mols	',
+    },
+    {
+      name: 'Creatives	',
+      tutor: 'P kelechi',
+    },
+  ]
+  const navigate = useNavigate()
+  return (
+    <div className='App'>
+      <div className='hero'>
+        <div className='head'>
+          <img src={Logo} alt='' className='two' />
+          <img src={Teen} alt='' className='one' />
+        </div>
+        <div className='texts'>
+          <div className='cover'>
+            <h1>Unleash Your Faith</h1>
+            <p>NAVIGATE CONFRENCE 2023</p>
+
+            <button onClick={() => navigate('/register')}>Register</button>
+          </div>
+        </div>
+      </div>
+      <div className='about'>
+        <div className='left'>
+          <img
+            src='https://images.unsplash.com/photo-1455849318743-b2233052fcff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80'
+            alt=''
+          />
+        </div>
+        <div className='right'>
+          <h1>About Navigate</h1>
+          <p>
+            The Navigate Conference is the Elevation Church’s (TEC) annual
+            conference organized to make spiritual, emotional and mental
+            investments in the teenage demography, teaching them life and
+            vocational skills. The programme is designed to address the
+            developmental gap within the teenage community. The conference
+            addresses key issues like self-concept/esteem, leadership, nation
+            building and personal & group productivity in addition to skill
+            development sessions that give participants the opportunity to learn
+            a new skill.
+          </p>
+        </div>
+      </div>
+      <div className='locat'>
+        <div className='left'>
+          <div>
+            <div className='cover'>
+              <div className='one'>
+                <h1>Sunday, July 25th</h1>
+                <div className='linee'>
+                  <div className='line'></div>
+                </div>
+                <h1>Friday, July 28th</h1>
+              </div>
+              <div className='two'>
+                <h1>2</h1>
+                <h1>0</h1>
+                <h1>2</h1>
+                <h1>3</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='right'>
+          <img src={Teen2} alt='' />
+        </div>
+      </div>
+      <div className='vocations'>
+        <div className='head'>
+          <h1>Vocational Skills available</h1>
+        </div>
+        <div className='cards'>
+          {List.map((item) => (
+            <div className='card' key={item.name}>
+              <h2>{item.name}</h2>
+              <p>by</p>
+              <h4>{item.tutor}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='rsvp'>
+        <div className='cover'>
+          <div className='rs'>
+            <h1>RS</h1>
+          </div>
+          <div className='content'>
+            <p>Let us know you'll be there</p>
+            <img src={Frame} alt='' />
+          </div>
+          <div className='vp'>
+            <h1>VP</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Register.js
@@ -0,0 +1,204 @@
+import { useState } from 'react'
+import './App.scss'
+import Logo from './images/logo.jpg'
+import Teen from './images/teen.jpg'
+
+function Register() {
+  const [childInfo, setChildInfo] = useState([
+    {
+      firstName: '',
+      lastName: '',
+      gender: '',
+      age: '',
+      class: '',
+      email: '',
+      phone: '',
+    },
+  ])
+  const handleAddressChange = (event, index) => {
+    let data = [...childInfo]
+    data[index][event.target.name] = event.target.value
+    setChildInfo(data)
+  }
+  const addAddressFields = () => {
+    let object = {
+      firstName: '',
+      lastName: '',
+      gender: '',
+      age: '',
+      class: '',
+      email: '',
+      phone: '',
+    }
+    setChildInfo([...childInfo, object])
+  }
+  const removeAddressFields = (index) => {
+    let data = [...childInfo]
+    data.splice(index, 1)
+    setChildInfo(data)
+  }
+  const openPDF = () => {
+    window.open('./images/camprules.pdf', '_blank')
+  }
+  return (
+    <div className='register'>
+      <div className='head'>
+        <img src={Logo} alt='' className='two' />
+        <img src={Teen} alt='' className='one' />
+      </div>
+      <div className='form-head'>
+        <h1>Camp Registration Form</h1>
+        <p>Please fill the form below to register</p>
+      </div>
+      <div className='form'>
+        <div className='cover'>
+          <div className='tag'>
+            <h2>Parent information</h2>
+          </div>
+          <div className='form-body'>
+            <div className='card'>
+              <p>First Name</p>
+              <input type='text' placeholder='Input First Name' />
+            </div>
+            <div className='card'>
+              <p>Last Name</p>
+              <input type='text' placeholder='Input Last Name' />
+            </div>
+            <div className='card'>
+              <p>Email Address</p>
+              <input type='email' placeholder='Input Email Address' />
+            </div>
+            <div className='card'>
+              <p>Phone Number</p>
+              <input type='text' placeholder='Input Phone Number' />
+            </div>
+            <div className='card'>
+              <p>Are you a member of the Elevation Church?</p>
+              <select name='' id=''>
+                <option value=''>-select-</option>
+                <option value=''>-Yes-</option>
+                <option value=''>-No-</option>
+              </select>
+            </div>
+            <div className='card'>
+              <p>Address</p>
+              <input type='text' placeholder='Input Address' />
+            </div>
+          </div>
+          <div className='tag2'>
+            <h2>Teenager's Information</h2>
+          </div>
+          {childInfo.map((form, index) => (
+            <div className='form-body'>
+              <div className='card'>
+                <p>First Name</p>
+                <input
+                  type='text'
+                  placeholder='Input First Name'
+                  onChange={(event) => handleAddressChange(event, index)}
+                  defaultValue={form.firstName}
+                />
+              </div>
+              <div className='card'>
+                <p>Last Name</p>
+                <input
+                  type='text'
+                  placeholder='Input Last Name'
+                  onChange={(event) => handleAddressChange(event, index)}
+                  defaultValue={form.lastName}
+                />
+              </div>
+              <div className='card'>
+                <p>Gender</p>
+                <select
+                  name=''
+                  id=''
+                  onChange={(event) => handleAddressChange(event, index)}
+                  defaultValue={form.gender}
+                >
+                  <option value=''>-select-</option>
+                  <option value=''>Female</option>
+                  <option value=''>Male</option>
+                </select>
+              </div>
+              <div className='card'>
+                <p>Age range</p>
+                <select
+                  name=''
+                  id=''
+                  onChange={(event) => handleAddressChange(event, index)}
+                  defaultValue={form.age}
+                >
+                  <option value=''>-select-</option>
+                  <option value=''>13 - 14 years</option>
+                  <option value=''>15 - 16 years</option>
+                  <option value=''>17 - 19 years</option>
+                </select>
+              </div>
+              <div className='card'>
+                <p>Email Address</p>
+                <input
+                  type='email'
+                  placeholder='Input Email Address'
+                  onChange={(event) => handleAddressChange(event, index)}
+                  defaultValue={form.email}
+                />
+              </div>
+              <div className='card'>
+                <p>Phone Number</p>
+                <input
+                  type='tel'
+                  placeholder='Input Phone Number'
+                  onChange={(event) => handleAddressChange(event, index)}
+                  defaultValue={form.phone}
+                />
+              </div>
+              <div className='card'>
+                <p>Vocational class</p>
+                <select
+                  name=''
+                  id=''
+                  onChange={(event) => handleAddressChange(event, index)}
+                  defaultValue={form.class}
+                >
+                  <option value=''>-select-</option>
+                  <option value=''>-Yes-</option>
+                  <option value=''>-No-</option>
+                </select>
+              </div>
+              <div className='long'>
+                <div
+                  className='remove'
+                  onClick={() => removeAddressFields(index)}
+                >
+                  <p>remove</p>
+                </div>
+              </div>
+            </div>
+          ))}
+          <div className='lons'>
+            <div className='add' onClick={addAddressFields}>
+              <p>add attendee</p>
+            </div>
+          </div>
+          <div className='accept'>
+            <input type='checkbox' name='' id='' />
+            <p>I agree that i have read the </p>
+            <a
+              href='https://drive.google.com/file/d/1SX4HAL2o64MvpXpb6QJTfqDjQvA88yqc/view?usp=sharing'
+              target='_blank'
+            >
+              <p>camp rules </p>
+            </a>
+            and i consent that i'm allowing my child/ward to attend.
+          </div>
+          <div className='submit'>
+            <button>Proceed</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
